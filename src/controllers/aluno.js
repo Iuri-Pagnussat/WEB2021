ffa7037d9c6b959ce4df98bfc5adfdee6d479d73
Iuri-Pagnussat <74273006+Iuri-Pagnussat@users.noreply.github.com>
2021-08-19 21:40:28 -0300

    novas rotas
    novas rotas

--- src/controllers/aluno.js
+++ src/controllers/aluno.js
@@ -2,7 +2,7 @@ let alunoService = require('../services/aluno')
 
 const getAlunos = async (req, res, next) => {
     try {
-        await alunoService.getAluno(req.params)
+        await alunoService.getAluno()
         .then(ret => res.status(201).send(ret))
         .catch(err => res.status(500).send(err))
     } catch (err) {
@@ -10,4 +10,53 @@ const getAlunos = async (req, res, next) => {
     }
 }
 
+const postAlunos = async (req, res, next) => {
+    try {
+        await alunoService.postAlunos(req.body)
+        .then(ret => res.status(201).send(ret))
+        .catch(err => res.status(500).send(err))
+    } catch (err) {
+        next(err);
+    }
+}
+
+const deleteAlunos = async (req, res, next) => {
+    try {
+        await alunoService.deleteAlunos(req.params)
+        .then(ret => res.status(204).send(ret))
+        .catch(err => res.status(500).send(err))
+    } catch (err) {
+        next(err);
+    }
+}
+
+const putAlunos = async (req, res, next) => {
+    try {
+        let params = req.body
+        params.id = req.params.id
+        await alunoService.putAlunos(params)
+        .then(ret => res.status(200).send(ret))
+        .catch(err => res.status(500).send(err))
+    } catch (err) {
+        next(err);
+    }
+}
+
+const patchAlunos = async (req, res, next) => {
+    try {
+        let params = req.body
+        params.id = req.params.id
+        await alunoService.patchAlunos(params)
+        .then(ret => res.status(200).send(ret))
+        .catch(err => res.status(500).send(err))
+    } catch (err) {
+        next(err);
+    }
+}
+
+
+module.exports.postAlunos = postAlunos
 module.exports.getAlunos = getAlunos
+module.exports.deleteAlunos = deleteAlunos
+module.exports.putAlunos = putAlunos
+module.exports.patchAlunos = patchAlunos
